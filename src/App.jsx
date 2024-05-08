6f68603d4c69c0893a2078df4aa5fcc5b0d2d6a9
Love <malujalove1508@gmail.com>
2024-05-08 22:16:05 +0530

    Created Eye Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import LandingPage from './components/LandingPage'
 import Marquee from './components/Marquee'
 import About from './components/about'
+import Eyes from './components/Eyes'
 function App() {
   return (
     <div className='w-full h-screen bg-zinc-900 text-white'>
@@ -10,6 +11,7 @@ function App() {
       <LandingPage/>
       <Marquee/>
       <About />
+      <Eyes/>
       </div>
   )
 }
