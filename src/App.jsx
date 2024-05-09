c24e8dd208f8ef09a15ac1a9de7f7bbf0120b5c9
Manan Bhasin <mananbhasin5657@gmail.com>
2024-05-09 10:00:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import LandingPage from './components/LandingPage'
-import Marquee from './components/Marquee'
-import About from './components/about'
-import Eyes from './components/Eyes'
+import React from "react";
+import Navbar from "./components/Navbar";
+import LandingPage from "./components/LandingPage";
+import Marquee from "./components/Marquee";
+import About from "./components/About";
+import Eyes from "./components/Eyes";
+import Featured from "./components/Featured";
+import Cards from "./components/Cards";
+import Footer from "./components/Footer";
+import LocomotiveScroll from "locomotive-scroll";
+
 function App() {
+  const locomotiveScroll = new LocomotiveScroll();
   return (
-    <div className='w-full h-screen bg-zinc-900 text-white'>
-      <Navbar/>
-      <LandingPage/>
-      <Marquee/>
+    <div className="w-full min-h-screen text-white bg-zinc-900">
+      <Navbar />
+      <LandingPage />
+      <Marquee />
       <About />
-      <Eyes/>
-      </div>
-  )
+      <Eyes />
+      <Featured />
+      <Cards />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
