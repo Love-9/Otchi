56ac929ffa079a161f84be7f15f5db566621690c
manan37 <mananbhasin5657@gmail.com>
2024-05-09 09:30:36 +0530

    Final Commit

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -46,8 +46,8 @@ function LandingPage() {
     <div
       data-scroll
       data-scroll-section
-      data-scroll-speed="-.20"
-      className="w-full h-screen bg-black pt-1" 
+      data-scroll-speed="-.3"
+      className="w-full h-screen bg-zinc-900 pt-1"
     >
       <div className="textHead mt-40 px-[4.5vw] ">
         {["we Create", "Eye-Opening", "Presentations"].map((item, index) => {
@@ -59,11 +59,11 @@ function LandingPage() {
                     initial={{ width: 0 }}
                     animate={{ width: "8vw" }}
                     transition={{ ease: [0.76, 0, 0.24, 1], duration: 1 }}
-                    className=" mr-[0.5vw] w-[8vw] h-[5.2vw] 
-                  relative -top-[-0vw] bg-cover bg-center bg-[url('https://ochi.design/wp-content/uploads/2022/04/content-image01.jpg')] rounded-md"
+                    className=" mr-[0.5vw] w-[8vw] h-[5.2vw]
+                  relative -top-[.2vw] bg-cover bg-center bg-[url('https://ochi.design/wp-content/uploads/2022/04/content-image01.jpg')] rounded-md"
                   ></motion.div>
                 )}
-                <h1 className=" flex items-center uppercase text-[7vw] h-full  leading-[6vw]  font-bold tracking-tighter">
+                <h1 className=" flex items-center uppercase text-[8.8vw] h-full  leading-[6.7vw] font-bold font-['Test_Founders_Grotesk_X-Condensed'] tracking-normal">
                   {item}
                 </h1>
               </div>
@@ -71,21 +71,24 @@ function LandingPage() {
           );
         })}
       </div>
-      <div className="border-t-[1px] border-zinc-800 mt-32 flex justify-between items-center py-5 px-[4.5vw]">
+      <div className=" border-t-[1px] border-zinc-800 mt-48 flex justify-between items-center py-5 px-[4.5vw]">
         {[
           "For public and private companies",
           "From the First pitch to IPO",
         ].map((item, index) => (
-          <p className="text-md font-normal tracking-tight leading-none">
+          <p className=" text-xl font-normal tracking-tight leading-none font-['Neue_Montreal']">
             {item}
           </p>
         ))}
         <div className="start flex  items-center gap-4">
-          <div className="group hover:bg-black   px-[0.5vw] py-[0.3vw] border-[1.5px] border-white font-light  rounded-full">
-            <h1 className="group-hover:text-white "> START THE PROJECT</h1>
+          <div className="group hover:bg-white hover:cursor-pointer  transition-all duration-500 px-[0.5vw] py-[0.3vw] border-[1.5px] border-zinc-400 font-light  rounded-full">
+            <h1 className="group-hover:text-black font-['Neue_Montreal']">
+              {" "}
+              START THE PROJECT
+            </h1>
           </div>
-          <div className=" group hover:bg-black w-[2.4vw] h-[2.4vw] flex items-center justify-center  border-[1.5px]  border-black rounded-full">
-            <span className=" group-hover:text-white rotate-[45deg]  ">
+          <div className=" group hover:bg-white hover:cursor-pointer hover:scale-110 transition-all duration-500 w-[2.3vw] h-[2.3vw] flex items-center justify-center  border-[1.5px]  border-zinc-400 rounded-full">
+            <span className=" group-hover:text-black rotate-[45deg] text-lg">
               <FaArrowUpLong />
             </span>
           </div>
--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -2,20 +2,19 @@ import { motion } from "framer-motion";
 import React from "react";
 
 function Marquee() {
-  motion;
   return (
     <div
       data-scroll
       data-scroll-section
       data-scroll-speeed=".2"
-      className="w-full pt-20 py-15 rounded-t-3xl bg-[#004d43] text-white"
+      className="w-full relative pt-20 pb-10 py-15 rounded-t-3xl bg-[#004d43] text-white"
     >
-      <div className="movingText border-t-2 border-b-2 border-zinc-300  flex   overflow-hidden whitespace-nowrap ">
+      <div className="movingText border-t-[.8px] border-b-[.8px] border-zinc-300  flex  overflow-hidden whitespace-nowrap ">
         <motion.h1
           initial={{ x: "0" }}
           animate={{ x: "-100%" }}
           transition={{ repeat: Infinity, ease: "linear", duration: 10 }}
-          className="text-[17.5vw] leading-none font-[fantasy] pr-20 pt-10 -mb-[7vw] "
+          className="text-[22vw] leading-none font-['Test_Founders_Grotesk_X-Condensed'] pr-40 -mb-[7vw] -mt-5 "
         >
           WE ARE OCHI
         </motion.h1>
@@ -23,7 +22,15 @@ function Marquee() {
           initial={{ x: "0" }}
           animate={{ x: "-100%" }}
           transition={{ repeat: Infinity, ease: "linear", duration: 10 }}
-          className="text-[17.5vw] leading-none font-[fantasy] pr-20 pt-10 -mb-[0vw] "
+          className="text-[22vw] leading-none font-['Test_Founders_Grotesk_X-Condensed'] pr-40 -mb-[0vw] -mt-5"
+        >
+          WE ARE OCHI
+        </motion.h1>
+        <motion.h1
+          initial={{ x: "0" }}
+          animate={{ x: "-100%" }}
+          transition={{ repeat: Infinity, ease: "linear", duration: 10 }}
+          className="text-[20vw] leading-none font-['Test_Founders_Grotesk_X-Condensed'] pr-40 -mb-[0vw] -mt-5"
         >
           WE ARE OCHI
         </motion.h1>
